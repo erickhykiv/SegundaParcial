3f3dd96ddf50f7a3c368fcda60b8b349244cf3b4
Erick <Erick@DESKTOP-84G70F9>
2024-10-21 19:12:12 -0600

    Agregar archivos de proyecto.

---
+++ SegundaParcial/SegundaParcial/Modelos/Producto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SegundaParcial.Modelos
+{
+    public class Producto
+    {
+        public int NumeroInventario { get; set; }
+        public string Nombre { get; set; }
+        public string Departamento { get; set; }
+    }
+}
